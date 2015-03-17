9016a5924875818f7cd610b9474e6f918d151921
Brett Aldrich <brett@reelrobotics.com>
2015-03-17 01:19:53 -0700

    sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+#include "RoboteqDevice.h"
+#include "ErrorCodes.h"
+#include "Constants.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string response = "";
+	RoboteqDevice device;
+	int status = device.Connect("/dev/ttyS0");
+
+	if(status != RQ_SUCCESS)
+	{
+		cout<<"Error connecting to device: "<<status<<"."<<endl;
+		return 1;
+	}
+
+	cout<<"- SetConfig(_DINA, 1, 1)...";
+	if((status = device.SetConfig(_DINA, 1, 1)) != RQ_SUCCESS)
+		cout<<"failed --> "<<status<<endl;
+	else
+		cout<<"succeeded."<<endl;
+
+	//Wait 10 ms before sending another command to device
+	sleepms(10);
+
+	int result;
+	cout<<"- GetConfig(_DINA, 1)...";
+	if((status = device.GetConfig(_DINA, 1, result)) != RQ_SUCCESS)
+		cout<<"failed --> "<<status<<endl;
+	else
+		cout<<"returned --> "<<result<<endl;
+
+	//Wait 10 ms before sending another command to device
+	sleepms(10);
+
+	cout<<"- GetValue(_ANAIN, 1)...";
+	if((status = device.GetValue(_ANAIN, 1, result)) != RQ_SUCCESS)
+		cout<<"failed --> "<<status<<endl;
+	else
+		cout<<"returned --> "<<result<<endl;
+
+	//Wait 10 ms before sending another command to device
+	sleepms(10);
+
+	cout<<"- SetCommand(_GO, 1, 1)...";
+	if((status = device.SetCommand(_GO, 1, 1)) != RQ_SUCCESS)
+		cout<<"failed --> "<<status<<endl;
+	else
+		cout<<"succeeded."<<endl;
+
+	device.Disconnect();
+	return 0;
+}
