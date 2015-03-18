017aa26df3115d47e5fb2a1672d1fc1339e355bc
Brett Aldrich <brett@reelrobotics.com>
2015-03-18 10:47:54 -0700

    Oops

--- RoboteqDevice.cpp
+++ RoboteqDevice.cpp
@@ -115,7 +115,7 @@ void RoboteqDevice::InitPort()
 	newtio.c_cflag &= ~PARODD;		/* Select the Even Parity (Disabled) through Control options*/
 	newtio.c_cflag &= ~CSTOPB;		/*Set number of Stop Bits to 1*/
 
-	//Timout Parameters. Set to 2 characters (VMIN) and 2 second (VTIME) timeout.
+	//Timout Parameters. Set to 2 characters (VMIN) and 2 second (VTIME) timeout*/
 	newtio.c_cc[VMIN] = 2;
 	newtio.c_cc[VTIME] = 20;
 	
