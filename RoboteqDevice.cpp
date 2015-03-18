aba32a2534ff1f6a0b4660c9631d4f6e1f34f4d3
Brett Aldrich <brett@reelrobotics.com>
2015-03-18 10:31:52 -0700

    Cleaned Up Comments

--- RoboteqDevice.cpp
+++ RoboteqDevice.cpp
@@ -116,7 +116,7 @@ void RoboteqDevice::InitPort()
 	newtio.c_cflag &= ~CSTOPB;		/*Set number of Stop Bits to 1
 
 	
-	//cwrset.c_cc[VMIN] = 6;
+	
 
 /* Flush the Input buffer and set the attribute NOW without waiting for Data to Complete*/
 	tcflush (handle, TCIFLUSH);
